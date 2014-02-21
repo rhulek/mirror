f521a21ec48510985dfdc167e42c4ab2f9b3c161
Richard Hulek <hulek@iba.muni.cz>
2014-02-21 23:31:27 +0100

    records

--- R/ss.R
+++ R/ss.R
@@ -1,6 +1,6 @@
 ss<-function(records,centralValueType="median",whiskerValueType="5_95",transformationType="none") {
   
-  return(records);
+  return(nrow(records));
   
   library(alldatabrowser)
   library(genasis)  

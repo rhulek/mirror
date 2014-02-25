3888e189c83a898ddcc33be26a93324637c43eb7
Richard Hulek <hulek@iba.muni.cz>
2014-02-25 21:44:07 +0100

    validace R balicku, aby sel nainstalovat

---
+++ R/ts.R
@@ -0,0 +1,7 @@
+ts <- function(records, centralValueType="median", whiskerValueType="5_95", transformationType="none") {  
+  
+  library(alldatabrowser)
+  library(genasis)  
+  
+  return(length(x=records));
+}

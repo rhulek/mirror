e49dfd93ddc7869102d64acf5eb4d123aaf579fa
Jiri Kalina <kalina@iba.muni.cz>
2014-02-27 16:07:02 +0100

    ts oprava dateTimestring -> dateTimeString

--- R/ts.R
+++ R/ts.R
@@ -1,20 +1,9 @@
-# timeSeriesvalues[[1]]$value
-# class(timeSeriesvalues)
-
-
-
-# Vytvori datovou strukturu pro report TimeSeries
-#
-# Pro kazdou casovou radu provede:
-# 1. setrideni hodnot podle data
-# 2. identifikace casovych der a podle toho rozpad do samostanych useku casove rady
-# 3. Vypocte trend summary pro kazdou casouvou radu
-# 4. Provede rocni agregaci casovych rad
-# 5. Transformuje vysledky do datove struktury TimeSeriesDataSeries
-ts <- function(records, centralValueType="median", whiskerValueType="5_95", transformationType="none") {  
+timeSeries<-function(records,centralValueType="median",whiskerValueType="5_95",transformationType="none") {  
   
-  library(alldatabrowser)
-  library(genasis)  
+  library(genasis)
+  
+  ## Celkova obalka
+  allSeries<-list()
   
   ## Prvni opakovani cyklu - primarni casove rady
   seriesSets<-list()
@@ -22,21 +11,26 @@ ts <- function(records, centralValueType="median", whiskerValueType="5_95", tran
   # i cyklus bezi pres sites
   for (i in 1:length(records)) {
     loca<-as.character(records[[i]]$rowLabel)
-    value        <-c()
-    loqValue     <-c()
-    loqMethodCode<-c()
-    unit         <-c()
-    dateTime     <-c()
-    timeLength   <-c()
+    value         <-c()
+    loqValue      <-c()
+    loqMethodCode <-c()
+    unit          <-c()
+    dateTime      <-c()
+    dateTimeString<-c()
+    timeLength    <-c()
     
     for (j in 1:length(records[[i]]$values)) {
-      value        <-c(value,        records[[i]]$values[[j]]$value)
-      loqValue     <-c(loqValue,     records[[i]]$values[[j]]$loqValue)
-      loqMethodCode<-c(loqMethodCode,records[[i]]$values[[j]]$loqMethodCode)
-      unit         <-c(unit,         records[[i]]$values[[j]]$unit)
-      dateTime     <-c(dateTime,     records[[i]]$values[[j]]$dateTime)
-      timeLength   <-c(timeLength,   records[[i]]$values[[j]]$timeLength)
+      value         <-c(value,         records[[i]]$values[[j]]$value)
+      loqValue      <-c(loqValue,      records[[i]]$values[[j]]$loqValue)
+      loqMethodCode <-c(loqMethodCode, records[[i]]$values[[j]]$loqMethodCode)
+      unit          <-c(unit,          records[[i]]$values[[j]]$unit)
+      dateTime      <-c(dateTime,      records[[i]]$values[[j]]$dateTime)
+      dateTimeString<-c(dateTimeString,records[[i]]$values[[j]]$dateTimeString)
+      timeLength    <-c(timeLength,    records[[i]]$values[[j]]$timeLength)
     }
+    
+    dateTime<-as.Date(dateTimeString)
+    data<-data[order(dateTimeString),]
     
     # Nahrada LoQ (v promenne valu budou hodnoty vstupujici do vypoctu)
     valu<-value
@@ -132,21 +126,26 @@ ts <- function(records, centralValueType="median", whiskerValueType="5_95", tran
   # i cyklus bezi pres sites
   for (i in 1:length(records)) {
     loca<-as.character(records[[i]]$rowLabel)
-    value        <-c()
-    loqValue     <-c()
-    loqMethodCode<-c()
-    unit         <-c()
-    dateTime     <-c()
-    timeLength   <-c()
+    value         <-c()
+    loqValue      <-c()
+    loqMethodCode <-c()
+    unit          <-c()
+    dateTime      <-c()
+    dateTimeString<-c()
+    timeLength    <-c()
     
     for (j in 1:length(records[[i]]$values)) {
-      value        <-c(value,        records[[i]]$values[[j]]$value)
-      loqValue     <-c(loqValue,     records[[i]]$values[[j]]$loqValue)
-      loqMethodCode<-c(loqMethodCode,records[[i]]$values[[j]]$loqMethodCode)
-      unit         <-c(unit,         records[[i]]$values[[j]]$unit)
-      dateTime     <-c(dateTime,     records[[i]]$values[[j]]$dateTime)
-      timeLength   <-c(timeLength,   records[[i]]$values[[j]]$timeLength)
+      value         <-c(value,         records[[i]]$values[[j]]$value)
+      loqValue      <-c(loqValue,      records[[i]]$values[[j]]$loqValue)
+      loqMethodCode <-c(loqMethodCode, records[[i]]$values[[j]]$loqMethodCode)
+      unit          <-c(unit,          records[[i]]$values[[j]]$unit)
+      dateTime      <-c(dateTime,      records[[i]]$values[[j]]$dateTime)
+      dateTimeString<-c(dateTimeString,records[[i]]$values[[j]]$dateTimeString)
+      timeLength    <-c(timeLength,    records[[i]]$values[[j]]$timeLength)
     }
+    
+    dateTime<-as.Date(dateTimeString)
+    data<-data[order(dateTimeString),]
     
     # Nahrada LoQ (v promenne valu budou hodnoty vstupujici do vypoctu)
     valu<-value
@@ -159,7 +158,7 @@ ts <- function(records, centralValueType="median", whiskerValueType="5_95", tran
     
     # Jednotky musi byt stejne
     if (length(unique(unit))>1) {
-      stop()
+      stop("Units of some records differ!")
     } else {
       unit<-unique(unit)
     }
@@ -254,21 +253,26 @@ ts <- function(records, centralValueType="median", whiskerValueType="5_95", tran
   # i cyklus bezi pres sites
   for (i in 1:length(records)) {
     loca<-as.character(records[[i]]$rowLabel)
-    value        <-c()
-    loqValue     <-c()
-    loqMethodCode<-c()
-    unit         <-c()
-    dateTime     <-c()
-    timeLength   <-c()
+    value         <-c()
+    loqValue      <-c()
+    loqMethodCode <-c()
+    unit          <-c()
+    dateTime      <-c()
+    dateTimeString<-c()
+    timeLength    <-c()
     
     for (j in 1:length(records[[i]]$values)) {
-      value        <-c(value,        records[[i]]$values[[j]]$value)
-      loqValue     <-c(loqValue,     records[[i]]$values[[j]]$loqValue)
-      loqMethodCode<-c(loqMethodCode,records[[i]]$values[[j]]$loqMethodCode)
-      unit         <-c(unit,         records[[i]]$values[[j]]$unit)
-      dateTime     <-c(dateTime,     records[[i]]$values[[j]]$dateTime)
-      timeLength   <-c(timeLength,   records[[i]]$values[[j]]$timeLength)
+      value         <-c(value,         records[[i]]$values[[j]]$value)
+      loqValue      <-c(loqValue,      records[[i]]$values[[j]]$loqValue)
+      loqMethodCode <-c(loqMethodCode, records[[i]]$values[[j]]$loqMethodCode)
+      unit          <-c(unit,          records[[i]]$values[[j]]$unit)
+      dateTime      <-c(dateTime,      records[[i]]$values[[j]]$dateTime)
+      dateTimeString<-c(dateTimeString,records[[i]]$values[[j]]$dateTimeString)
+      timeLength    <-c(timeLength,    records[[i]]$values[[j]]$timeLength)
     }
+    
+    dateTime<-as.Date(dateTimeString)
+    data<-data[order(dateTimeString),]
     
     # Nahrada LoQ (v promenne valu budou hodnoty vstupujici do vypoctu)
     valu<-value
@@ -344,16 +348,21 @@ ts <- function(records, centralValueType="median", whiskerValueType="5_95", tran
     loqMethodCode<-c()
     unit         <-c()
     dateTime     <-c()
+    dateTimeString<-c()
     timeLength   <-c()
     
     for (j in 1:length(records[[i]]$values)) {
-      value        <-c(value,        records[[i]]$values[[j]]$value)
-      loqValue     <-c(loqValue,     records[[i]]$values[[j]]$loqValue)
-      loqMethodCode<-c(loqMethodCode,records[[i]]$values[[j]]$loqMethodCode)
-      unit         <-c(unit,         records[[i]]$values[[j]]$unit)
-      dateTime     <-c(dateTime,     records[[i]]$values[[j]]$dateTime)
-      timeLength   <-c(timeLength,   records[[i]]$values[[j]]$timeLength)
+      value         <-c(value,         records[[i]]$values[[j]]$value)
+      loqValue      <-c(loqValue,      records[[i]]$values[[j]]$loqValue)
+      loqMethodCode <-c(loqMethodCode, records[[i]]$values[[j]]$loqMethodCode)
+      unit          <-c(unit,          records[[i]]$values[[j]]$unit)
+      dateTime      <-c(dateTime,      records[[i]]$values[[j]]$dateTime)
+      dateTimeString<-c(dateTimeString,records[[i]]$values[[j]]$dateTimeString)
+      timeLength    <-c(timeLength,    records[[i]]$values[[j]]$timeLength)
     }
+    
+    dateTime<-as.Date(dateTimeString)
+    data<-data[order(dateTimeString),]
     
     # Nahrada LoQ (v promenne valu budou hodnoty vstupujici do vypoctu)
     valu<-value
@@ -361,7 +370,7 @@ ts <- function(records, centralValueType="median", whiskerValueType="5_95", tran
     
     # Jednotky musi byt stejne
     if (length(unique(unit))>1) {
-      stop()
+      stop("Units of some records differ!")
     } else {
       unit<-unique(unit)
     }
@@ -428,16 +437,132 @@ ts <- function(records, centralValueType="median", whiskerValueType="5_95", tran
     labels<-as.list(c(labels,list(label)))
   }
   
+  ## Vypocet prostorove agregovane rady z jednotlivych rocnich agregaci (jen jednou pro cely datovy soubor)
+  numofrows<-length(seriesSets)/4
+  valu<-c()
+  data<-c()
+  unit<-c()
+  
+  for (i in 1:numofrows) {
+    lengthofrow<-length(seriesSets[[numofrows+i]]$series[[1]]$values)
+    
+    for (j in 1:lengthofrow) {
+      valu<-c(valu,seriesSets[[numofrows+i]]$series[[1]]$values[[j]]$centralValue)
+      data<-c(data,as.character(seriesSets[[numofrows+i]]$series[[1]]$values[[j]]$dateTimeString))
+      unit<-c(unit,as.character(seriesSets[[numofrows+i]]$series[[1]]$values[[j]]$unit))
+    }    
+  }
+  
+  # Jednotky musi byt stejne
+  if (length(unique(unit))>1) {
+    stop("Units of some records differ!")
+  } else {
+    unit<-unique(unit)
+  }
+  
+  whisk<-c("5_95","25_75","min_max","2iq","ci")
+  whisl<-c("quantile05","quantile25","min","iql","cil")
+  whisu<-c("quantile95","quantile75","max","iqu","ciu")
+  
+  centv<-c("mean","median","geomean")
+  centf<-c("arimean","quantile50","geomean")
+  
+  f1<-centf[which(centv==centralValueType)]
+  f2<-whisl[which(whisk==whiskerValueType)]
+  f3<-whisu[which(whisk==whiskerValueType)]
+  
+  # Rocni agregace do noveho data.frame aggr
+  year<-gendate(substr(data,1,4))
+  aggr<-data.frame(aggregate(valu,by=list(year),FUN=f1)[,2],
+                   unit,
+                   centralValueType,
+                   aggregate(valu,by=list(year),FUN=f3)[,2],
+                   aggregate(valu,by=list(year),FUN=f2)[,2],
+                   whiskerValueType,
+                   gendate(aggregate(valu,by=list(year),FUN=f1)[,1]),
+                   as.character(aggregate(valu,by=list(year),FUN=f1)[,1]),
+                   as.character(aggregate(valu,by=list(year),FUN=length)[,2]),
+                   as.character(aggregate(valu,by=list(year),FUN=loqlength)[,2]))
+  colnames(aggr)<-c("centralValue","unit","centralValueType","whiskerTopValue","whiskerBottomValue","whiskerType","dateTime","dateTimeString","n","nUnderLOQ")
+  
+  hole<-3*mean(aggr$dateTime[-1]-aggr$dateTime[-nrow(aggr)],trim=0.05)
+  
+  # k udava poradi segmentu jedne casove rady
+  k<-1
+  values<-list()
+  series<-list()
+  
+  # j cyklus bezi pres jednotliva mereni
+  for (j in 1:nrow(aggr)) {
+    aggValue<-list(label=paste0("all_aggregation"),
+                   n=aggr$n[j],
+                   nUnderLOQ=aggr$nUnderLOQ[j],
+                   unit=aggr$unit[j],
+                   centralValue=aggr$centralValue[j],
+                   centralValueType=aggr$centralValueType[j],
+                   whiskerTopValue=aggr$whiskerTopValue[j],
+                   whiskerBottomValue=aggr$whiskerBottomValue[j],
+                   whiskerType=aggr$whiskerType[j],
+                   dateTimeString=aggr$dateTimeString[j]);
+    
+    if (j==1) {
+      values<-as.list(c(values,list(aggValue)))
+    } else {
+      if ((aggr$dateTime[j]-aggr$dateTime[j-1])<hole) {
+        values<-as.list(c(values,list(aggValue)))
+      } else {
+        timeSeriesRecord<-list(values=values,label=paste0("all_aggr",i," part",k))
+        k<-k+1
+        series<-as.list(c(series,list(timeSeriesRecord)))
+        values<-list(aggValue)
+      }
+    }
+  }
+  timeSeriesRecord<-list(values=values,label=paste0("all_aggr",i," part",k))
+  series<-as.list(c(series,list(timeSeriesRecord)))
+  
+  # Popis celkove agregovane rady
+  if (k<2) {
+    res<-genstatistic(aggr$centralValue,aggr$dateTime)$res
+    
+    trendSummary<-list(delta=res$delta,
+                       mannKendall=res$"Mann-Kendall",
+                       mannKendallP=res$MKp,
+                       daniels=res$Daniels,
+                       danielsP=res$Dp,
+                       mean=res$mean,
+                       sd=res$sd,
+                       geomean=res$"geom. mean",
+                       gsd=res$"geom. sd",
+                       median=res$median,
+                       min=res$min,
+                       max=res$max,
+                       perc5=quantile05(aggr$centralValue),
+                       perc25=quantile25(aggr$centralValue),
+                       perc75=quantile75(aggr$centralValue),
+                       perc95=quantile95(aggr$centralValue),
+                       geoMean95CIUpperBound=res$"geom. mean"*res$"geom. sd"^qnorm(0.975),
+                       geoMean95CILowerBound=res$"geom. mean"*res$"geom. sd"^qnorm(0.025),
+                       mean95CIUpperBound=res$mean+qnorm(0.975)*res$sd,
+                       mean95CILowerBound=res$mean+qnorm(0.025)*res$sd)
+  }
+  else {
+    trendSummary<-NA
+  }
+  
+  seriesSet<-list(series=series,trendSummary=trendSummary,label="all_agr")
+  allAggregation<-list(seriesSet)
+  label<-"All_aggr"
+  aggregationLabels<-list(label)
   
   
-  timeSeriesDataSeries <- list(seriesSets=seriesSets,
-                               labels=labels)
+  ## Zabaleni vysledku
+  allSeries<-list(seriesSets=seriesSets,
+                  labels=labels,
+                  allAggregation=allAggregation,
+                  aggregationLabels=aggregationLabels)
   
-  return(timeSeriesDataSeries);
+  timeSeriesDataSeries<-list(allSeries=allSeries)
+  
+  return(timeSeriesDataSeries)
 }
-
-
-# Test
-x<-timeSeries(timeSeriesData)
-
-x[[1]][[16]][[1]][[1]][[2]]
